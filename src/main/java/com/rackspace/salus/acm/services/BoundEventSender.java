c9fe07bed3bc89dcf8cff0a619249e390b3030f3
Adam <harageth@gmail.com>
2019-09-13 11:08:08 -0700

    updated try/catch and changed response code

--- src/main/java/com/rackspace/salus/acm/services/BoundEventSender.java
+++ src/main/java/com/rackspace/salus/acm/services/BoundEventSender.java
@@ -64,9 +64,7 @@ public class BoundEventSender {
       final String key = KafkaMessageKeyBuilder.buildMessageKey(event);
       try {
         kafkaTemplate.send(topic, key, event).get();
-      } catch (InterruptedException e) {
-        throw new RuntimeKafkaException(e);
-      } catch (ExecutionException e) {
+      } catch (InterruptedException|ExecutionException e) {
         throw new RuntimeKafkaException(e);
       }
     }
